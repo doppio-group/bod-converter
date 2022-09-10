917a1cd9d4483b231347fd7c71fa37cd0d7a8e56
ajayyadukrishnan <ajayyadukrishnan@gmail.com>
2022-09-11 01:52:29 +0530

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `BOD Converter`,
+    description: `Tool to convert MEC mapping to BOD`,
+    author: `@ajayyadukrishnan`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
