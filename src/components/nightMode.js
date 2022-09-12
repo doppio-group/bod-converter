716b14c257472b5783e4366094f3f03824494878
ajayyadukrishnan <ajayyadukrishnan@gmail.com>
2022-09-12 14:19:03 +0530

    Added dark mode changes

--- src/components/nightMode.js
+++ src/components/nightMode.js
@@ -1,20 +1,76 @@
 import React from 'react'
 import { FaSun, FaMoon } from "react-icons/fa"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import 'font-awesome/css/font-awesome.min.css';
 import "./nightMode.css"
+import {
+    useComponentDidMount,
+    useComponentDidUpdate,
+    useComponentWillUnmount
+} from "./utils";
 
-function nightMode() {
+export const NightMode = () => {
+
+    useComponentDidMount(() => {
+        var defaultDarkMode = window.localStorage.getItem("darkMode");
+        var checkbox = document.getElementById('nightModeCheckBox');
+        
+        defaultDarkMode = defaultDarkMode === null || defaultDarkMode === undefined || defaultDarkMode === "false" ? false : true;
+        
+        console.log("Default Dark Mode: " + defaultDarkMode);
+
+        checkbox.checked = defaultDarkMode;
+
+        if (checkbox.checked) {
+            document.documentElement.style.setProperty('--color-background', 'var(--color-dark)');
+            document.documentElement.style.setProperty('--color-text', 'var(--color-light)');
+
+        } else {
+            document.documentElement.style.setProperty('--color-background', 'var(--color-light)');
+            document.documentElement.style.setProperty('--color-text', 'var(--color-dark)');
+
+        }
+    });
 
     return (
         <div className="nightModeSwitcher">
-            <input type="checkbox" className="checkbox" id="nightModeCheckBox" onChange={switcher} />
+            {/* <input type="checkbox" className="checkbox" id="nightModeCheckBox" onChange={switcher} />
             <label htmlFor="nightModeCheckBox" className="label">
-                {/* <i className="fas fa-moon"></i>
-                <i className='fas fa-sun'></i> */}
                 <FaMoon color='white' size={'15px'} />
                 <FaSun color='yellow' size={'15px'} />
                 <div className='ball' />
+            </label> */}
+            <input type="checkbox" id="nightModeCheckBox" className="checkbox" onChange={switcher} />
+            <label htmlFor="nightModeCheckBox" className="switch_bg">
+                <div className="switch_slider"></div>
+                <div className="switch_button">
+                    <FaMoon className='fa-solid faMoon' color='white' size={'25px'} />
+                    <FaSun className='fa-solid faSun' color='white' size={'25px'} />
+                    {/* <i className="fa-solid fa-moon"></i> */}
+                </div>
             </label>
+            {/* <label for="checkbox">
+                <input type="checkbox" name="nightModeCheckBox" id="nightModeCheckBox" />
+                <div className="toggle">
+                    <svg id="graphic" viewBox="0 0 132.29 132.29" xmlns="http://www.w3.org/2000/svg">
+                        <g id="sung">
+                            <path id="sun" d="m99.722 66.146a33.576 33.576 0 0 1-33.576 33.576 33.576 33.576 0 0 1-33.576-33.576 33.576 33.576 0 0 1 33.576-33.576 33.576 33.576 0 0 1 33.576 33.576" />
+                            <g id="sunshine" transform="matrix(.92602 0 0 .92602 -10.774 -58.541)">
+                                <path transform="matrix(0 .58145 -.59067 0 145.26 159.26)" d="m-19.4 32.282h-45.867l22.934-39.722z" />
+                                <path transform="matrix(-.41115 .41115 -.41766 -.41766 109.64 196.03)" d="m-19.4 32.282h-45.867l22.934-39.722z" />
+                                <path transform="matrix(-.58145 0 0 -.59067 58.451 196.84)" d="m-19.4 32.282h-45.867l22.934-39.722z" />
+                                <path transform="matrix(-.41115 -.41115 .41766 -.41766 21.685 161.22)" d="m-19.4 32.282h-45.867l22.934-39.722z" />
+                                <path transform="matrix(0 -.58145 .59067 0 20.875 110.03)" d="m-19.4 32.282h-45.867l22.934-39.722z" />
+                                <path transform="matrix(.41115 -.41115 .41766 .41766 56.495 73.269)" d="m-19.4 32.282h-45.867l22.934-39.722z" />
+                                <path transform="matrix(.58145 0 0 .59067 107.68 72.459)" d="m-19.4 32.282h-45.867l22.934-39.722z" />
+                                <path transform="matrix(.41115 .41115 -.41766 .41766 144.45 108.08)" d="m-19.4 32.282h-45.867l22.934-39.722z" />
+                            </g>
+                        </g>
+                        <path id="croissant" d="m55.461 34.281a33.587 33.587 0 0 0-22.874 31.832 33.587 33.587 0 0 0 33.587 33.587 33.587 33.587 0 0 0 30.441-19.395 31.187 31.187 0 0 1-17.268 5.2174 31.187 31.187 0 0 1-31.187-31.187 31.187 31.187 0 0 1 7.3022-20.053z" />
+                    </svg>
+
+                </div>
+            </label> */}
         </div>
 
     )
@@ -41,6 +97,3 @@ function switcher(event) {
         localStorage.setItem("darkMode", "false");
     }
 }
-
-
-export default nightMode
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,19 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
 import CustomForm from "../components/customForm"
-import NightMode from "../components/nightMode"
+import { NightMode } from "../components/nightMode"
 
 
 const IndexPage = () => (
-    
+
     <Layout>
         <Seo title="Home" />
         <div className={styles.textCenter}>
             <div>
-                {/* <StaticImage
-                    src="../images/example.png"
-                    loading="eager"
-                    width={64}
-                    quality={95}
-                    formats={["auto", "webp", "avif"]}
-                    alt=""
-                /> */}
                 <div className="nigthModeSwitchDiv">
                     <NightMode />
                 </div>
@@ -31,37 +21,7 @@ const IndexPage = () => (
             <h1>
                 <b>BOD Converter</b>
             </h1>
-            {/* <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
-        {samplePageLinks.map((link, i) => (
-          <React.Fragment key={link.url}>
-            <Link to={link.url}>{link.text}</Link>
-            {i !== samplePageLinks.length - 1 && <> · </>}
-          </React.Fragment>
-        ))}
-        <br />
-        Edit <code>src/pages/index.js</code> to update this page.
-      </p> */}
         </div>
-        {/* <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-          >
-            {link.text} ↗
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    {moreLinks.map((link, i) => (
-      <React.Fragment key={link.url}>
-        <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-        {i !== moreLinks.length - 1 && <> · </>}
-      </React.Fragment>
-    ))} */}
         <CustomForm />
     </Layout>
 )
@@ -73,6 +33,6 @@ const IndexPage = () => (
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home" />
+export const Head = () => <Seo title="BOD Converter" />
 
 export default IndexPage
